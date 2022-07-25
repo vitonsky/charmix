069f8e76eaa96c66ae049dc3da2f522018548503
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-07-26 01:16:23 +0300

    build: remove unnecessary packages

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,4 @@
-const fx = require('mkdir-recursive');
-
 import fs from 'fs/promises';
-
-export const mkdir = (path: string) =>
-	new Promise<void>((res, rej) => {
-		fx.mkdir(path, (err: any) => {
-			if (err) {
-				rej(err);
-			} else {
-				res();
-			}
-		});
-	});
 
 export const isResourceExist = (filename: string) =>
 	fs.access(filename).then(
