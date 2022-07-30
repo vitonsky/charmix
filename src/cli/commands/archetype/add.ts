6f10ed6abdc38cef86088c073c7350dcab95102f
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-07-31 00:59:19 +0300

    feat(#19): fetch archetype while adding

--- src/cli/commands/archetype/add.ts
+++ src/cli/commands/archetype/add.ts
@@ -1,34 +1,78 @@
+import { AppOptions } from '../..';
+import { ArchetypeReference } from '../../../archetype';
+import { ArchetypeManager } from '../../../archetype/ArchetypeManager';
 import { ArchetypesRegistry } from '../../../repository/ArchetypesRegistry';
 import { CriticalError } from '../../../utils';
 import { CommandHandlerConstructor, CommandsBuilder } from '../CliCommand';
 
 export const prepareArchetypeAdd: CommandHandlerConstructor<{
+	config: AppOptions;
 	registry: ArchetypesRegistry;
 }> =
-	({ registry }) =>
+	({ config, registry }) =>
 		async (args: Record<string, any>) => {
 			const { force = false, type, name, reference } = args;
 
+			const archetypeManager = new ArchetypeManager({ cacheDir: config.cacheDir });
 			const archetypes = await registry.getArchetypes();
 
-			// Find exists entries with same name and delete or exit program
-			const filteredArchetypes = archetypes.filter((archetype) => {
-				if (archetype.name !== name) return true;
-
+			// Exit if archetype already exists
+			const uniqueNameCheck = (name: string) => {
 				if (!force) {
-					const message = `Archetype "${args.name}" already exist\n\nIf you want to replace, run with option -f`;
-					throw new CriticalError(message);
+					const foundArchetype = archetypes.find(
+						(archetype) => archetype.name === name,
+					);
+					if (foundArchetype !== undefined) {
+						const message = `Archetype "${name}" already exist\n\nIf you want to replace, run with option -f`;
+						throw new CriticalError(message);
+					}
 				}
+			};
 
-				return false;
+			// check user provided name
+			uniqueNameCheck(name);
+
+			// Install
+			console.log('Fetch archetype...');
+			const temporaryArchetype = await archetypeManager.fetchArchetypeToTempDirectory({
+				type,
+				src: reference,
 			});
 
-			// Add
-			filteredArchetypes.push({ type: type as any, name, src: reference });
-			await registry.setArchetypes(filteredArchetypes);
+			// validations
+			// TODO: add name validation
+			const archetypeName = name ?? temporaryArchetype.manifest.name;
+			if (typeof archetypeName !== 'string') {
+				throw new CriticalError(
+					'Not found name in archetype manifest. Set name manually to successful install',
+				);
+			}
+
+			// check name from archetype manifest
+			uniqueNameCheck(archetypeName);
+
+			const archetypeRef: ArchetypeReference = {
+				name: archetypeName,
+				type,
+				src: reference,
+			};
+
+			console.log('Install archetype...');
+			// delete if exists
+			await archetypeManager.delete(archetypeRef);
+
+			// install from temporary directory
+			archetypeManager.install(archetypeRef, temporaryArchetype.path);
+
+			// Add to registry
+			const newArchetypesList = archetypes
+				.filter((archetype) => archetype.name !== archetypeName)
+				.concat(archetypeRef);
+
+			await registry.setArchetypes(newArchetypesList);
 		};
 
-export const buildArchetypeAdd: CommandsBuilder = async () => [
+export const buildArchetypeAdd: CommandsBuilder = async (config) => [
 	{
 		command: 'add',
 		description: 'Add archetype to registry',
@@ -42,11 +86,11 @@ export const buildArchetypeAdd: CommandsBuilder = async () => [
 				action: 'store_true',
 				help: 'force add archetype. If archetype with same name already exists, it will be replaced',
 			});
-			parser.add_argument('name');
+			parser.add_argument('--name', '-n');
 			parser.add_argument('reference');
 
 			const registry = new ArchetypesRegistry();
-			return prepareArchetypeAdd({ registry });
+			return prepareArchetypeAdd({ config, registry });
 		},
 	},
 ];
