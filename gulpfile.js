071d5f01af92214ce609881bfe7c680c8ed14edf
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-08-04 01:30:05 +0300

    build: replace paths in readme to github links

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const mergeStream = require('merge-stream');
 const clean = require('gulp-clean');
+const replace = require('gulp-replace');
 
 const cleanPackageJson = require('./scripts/gulp/cleanPackageJson');
 
 const buildDir = 'dist';
+const gitPublicMainPath = 'https://github.com/vitonsky/charmix/tree/master';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -27,8 +29,10 @@ function copyMetaFiles() {
 		mergeStream(
 			// Clean package.json
 			gulp.src(['./package.json']).pipe(cleanPackageJson()),
-			// Copy other
-			gulp.src(['./README.md', ...(isProduction ? [] : devFiles)], {
+			// Replace relative links to github links
+			gulp.src(['./README.md']).pipe(replace(/\.\//g, gitPublicMainPath + '/')),
+			// Copy additional files
+			gulp.src(isProduction ? [] : devFiles, {
 				allowEmpty: true,
 			}),
 		).pipe(gulp.dest(buildDir)),
