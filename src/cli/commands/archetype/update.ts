be18f79bda82a1964c7b91a561f4914b32ed0e50
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-08-13 22:36:07 +0300

    feat(#35): update archetypes by names

--- src/cli/commands/archetype/update.ts
+++ src/cli/commands/archetype/update.ts
@@ -7,9 +7,34 @@ export const prepareArchetypeUpdate: CommandHandlerConstructor<{
 	archetypesManager: ArchetypeManager;
 }> =
 	({ registry, archetypesManager }) =>
-		async () => {
+		async (args) => {
 			const archetypes = await registry.getArchetypes();
+
+			// Warn about not found archetypes and stop process
+			const selectedArchetypes = args.names as unknown as string[];
+			if (selectedArchetypes.length > 0) {
+				const notFoundArchetypes = selectedArchetypes.filter(
+					(name) =>
+						archetypes.findIndex((archetype) => archetype.name === name) === -1,
+				);
+
+				if (notFoundArchetypes.length > 0) {
+					notFoundArchetypes.forEach((name) => {
+						console.error(`Error: archetype "${name}" not found`);
+					});
+
+					process.exit(1);
+				}
+			}
+
 			for (const archetype of archetypes) {
+			// Skip not specified archetypes by name
+				if (
+					selectedArchetypes.length > 0 &&
+				!selectedArchetypes.includes(archetype.name)
+				)
+					continue;
+
 				try {
 					console.log(`Fetch archetype "${archetype.name}" ...`);
 					const temporaryArchetype =
@@ -30,7 +55,12 @@ export const buildArchetypeUpdate: CommandsBuilder = async ({ cacheDir }) => [
 	{
 		command: 'update',
 		description: 'Update archetypes',
-		handler: () => {
+		handler: ({ parser }) => {
+			parser.add_argument('names', {
+				nargs: '*',
+				help: 'archetype names to update',
+			});
+
 			const registry = new ArchetypesRegistry();
 			const archetypesManager = new ArchetypeManager({ cacheDir });
 			return prepareArchetypeUpdate({ registry, archetypesManager });
